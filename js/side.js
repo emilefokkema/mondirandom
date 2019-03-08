405e19c7d58ca63c9ca3e973c9e24c860b31491b
Emile Fokkema <emilefokkema@gmail.com>
2019-03-08 08:29:30 +0100

    make them pass

---
+++ js/side.js
@@ -0,0 +1,26 @@
+var f = function(require){
+	var Side = function(interval, position, direction){
+		this.interval = interval;
+		this.position = position;
+		this.direction = direction;
+		
+	};
+	Side.prototype.getOverlapWith = function(other){
+		if(other.direction != this.direction || other.position != this.position){
+			return null;
+		}
+		var overlappingInterval = this.interval.getOverlapWith(other.interval);
+		if(!overlappingInterval){
+			return null;
+		}
+		return new Side(overlappingInterval, this.position, this.direction);
+	};
+	return Side;
+}
+
+if(typeof module !== "undefined" && typeof require == "function"){
+	module.exports = f(require);
+}
+else if(typeof define == "function"){
+	define(f);
+}
