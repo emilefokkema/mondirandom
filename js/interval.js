2b00c1b221bdd84827ca17ebdee2fd7dd291266a
Emile Fokkema <emilefokkema@gmail.com>
2019-03-08 07:29:24 +0100

    make test pass

--- js/interval.js
+++ js/interval.js
@@ -16,6 +16,31 @@ var f = function(require){
 			intervals: [new Interval(this.from, newPoint, this.randomValueProvider), new Interval(newPoint, this.to, this.randomValueProvider)]
 		};
 	};
+	Interval.prototype.contains = function(point){
+		return point >= this.from && point <= this.to;
+	};
+	Interval.prototype.getOverlapWith = function(other){
+		var allPoints = [this.from, this.to, other.from, other.to];
+		var sorted = allPoints.sort();
+		var from, to;
+		for(var i = 0; i < 4; i++){
+			var point = sorted[i];
+			if(this.contains(point) && other.contains(point)){
+				if(from == undefined){
+					from = point;
+					continue;
+				}
+				if(to == undefined){
+					to = point;
+					break;
+				}
+			}
+		}
+		if(from !== undefined){
+			return new Interval(from, to, this.randomValueProvider);
+		}
+		return null;
+	};
 	Interval.around = function(point, radius, randomValueProvider){
 		return new Interval(point - radius, point + radius, randomValueProvider);
 	};
