a09fc46d7bdd828a128030a1ebc313f23abb3525
Emile Fokkema <emilefokkema@gmail.com>
2019-03-11 11:48:54 +0100

    improve current splitting tests and add test about neighbors

--- spec/fieldSplitterSpec.js
+++ spec/fieldSplitterSpec.js
@@ -5,10 +5,21 @@ describe("A FieldSplitter", function(){
 	var Direction = require("../js/direction.js");
 	var TestRandomValueProvider = require("./test-random-value-provider.js");
 	var instance;
-	var randomValueProvider;
+	
+	var splitFieldInDirection = function(field, direction, ratio){
+		var randomValueProvider = new TestRandomValueProvider();
+		randomValueProvider.randomField = field;
+		randomValueProvider.randomDirection = direction;
+		randomValueProvider.randomRatio = ratio;
+		instance.splitRandomField(randomValueProvider);
+	};
+	var drawFieldsInColors = function(fieldColors, context){
+		var randomValueProvider = new TestRandomValueProvider();
+		randomValueProvider.fieldColors = fieldColors;
+		instance.draw(context, randomValueProvider);
+	};
 
 	beforeEach(function(){
-		randomValueProvider = new TestRandomValueProvider();
 		instance = new FieldSplitter(10, 10, 1);
 	});
 
@@ -16,13 +27,44 @@ describe("A FieldSplitter", function(){
 		expect(instance).toBeTruthy();
 	});
 
+	describe("when it splits two fields", function(){
+		var bottomLeftField, topLeftField, rightField;
+
+		beforeEach(function(){
+			splitFieldInDirection(instance.fields[0], Direction.VERTICAL, 0.5);
+			var leftField = instance.fields.find(function(f){return f.rectangle.horizontalInterval.from == 0;});
+			splitFieldInDirection(leftField, Direction.HORIZONTAL, 0.5);
+			bottomLeftField = instance.fields.find(function(f){return f.rectangle.horizontalInterval.from == 0 && f.rectangle.verticalInterval.from == 0;});
+			topLeftField = instance.fields.find(function(f){return f.rectangle.horizontalInterval.from == 0 && f.rectangle.verticalInterval.from == 5;});
+			rightField = instance.fields.find(function(f){return f.rectangle.horizontalInterval.from == 5 ;});
+		});
+
+		it("should have three fields", function(){
+			expect(instance.fields.length).toBe(3);
+			expect(bottomLeftField).toBeTruthy();
+			expect(topLeftField).toBeTruthy();
+			expect(rightField).toBeTruthy();
+		});
+
+		xit("each field should have two neighbors", function(){
+			expect(topLeftField.neighbors.length).toBe(2);
+			expect(topLeftField.neighbors).toContain(bottomLeftField);
+			expect(topLeftField.neighbors).toContain(rightField);
+
+			expect(bottomLeftField.neighbors.length).toBe(2);
+			expect(bottomLeftField.neighbors).toContain(topLeftField);
+			expect(bottomLeftField.neighbors).toContain(rightField);
+
+			expect(rightField.neighbors.length).toBe(2);
+			expect(rightField.neighbors).toContain(topLeftField);
+			expect(rightField.neighbors).toContain(bottomLeftField);
+		});
+	});
+
 	describe("when it splits a random field", function(){
 
 		beforeEach(function(){
-			randomValueProvider.randomField = instance.fields[0];
-			randomValueProvider.randomDirection = Direction.VERTICAL;
-			randomValueProvider.randomRatio = 0.2;
-			instance.splitRandomField( randomValueProvider);
+			splitFieldInDirection(instance.fields[0], Direction.VERTICAL, 0.2);
 		});
 
 		it("should have two fields and one border", function(){
@@ -35,15 +77,18 @@ describe("A FieldSplitter", function(){
 			var fillStyleSpy;
 
 			beforeEach(function(){
-				randomValueProvider.randomColor = Color.RED;
 				fakeContext = new FakeContext();
 				spyOn(fakeContext,'fillRect');
 				fillStyleSpy = spyOnProperty(fakeContext, 'fillStyle', 'set');
-				instance.draw(fakeContext, randomValueProvider);
+				drawFieldsInColors([
+					{field: instance.fields[0], color: Color.RED},
+					{field: instance.fields[1], color: Color.BLUE}
+				], fakeContext);
 			});
 
 			it("should draw two rectangles", function(){
 				expect(fillStyleSpy).toHaveBeenCalledWith(Color.RED);
+				expect(fillStyleSpy).toHaveBeenCalledWith(Color.BLUE);
 				expect(fakeContext.fillRect).toHaveBeenCalledWith(0, 0, 2, 10);
 				expect(fakeContext.fillRect).toHaveBeenCalledWith(2, 0, 8, 10);
 				expect(fakeContext.fillRect).toHaveBeenCalledWith(1.5, 0, 1, 10);
--- spec/test-random-value-provider.js
+++ spec/test-random-value-provider.js
@@ -1,7 +1,7 @@
 var TestRandomValueProvider = function(){
 	this.randomField = undefined;
 	this.randomDirection = undefined;
-	this.randomColor = undefined;
+	this.fieldColors = [];
 	this.randomRatio = undefined;
 };
 TestRandomValueProvider.prototype.provideRandomField = function(fieldSplitter){
@@ -11,7 +11,13 @@ TestRandomValueProvider.prototype.provideRandomDirection = function(field){
 	return this.randomDirection;
 };
 TestRandomValueProvider.prototype.provideRandomColor = function(field, initialDistribution){
-	return this.randomColor;
+	for(var i=0;i<this.fieldColors.length;i++){
+		var fieldColor = this.fieldColors[i];
+		if(fieldColor.field == field){
+			return fieldColor.color;
+		}
+	}
+	throw 'No color provided for field';
 };
 TestRandomValueProvider.prototype.provideRandomRatio = function(){
 	return this.randomRatio;
