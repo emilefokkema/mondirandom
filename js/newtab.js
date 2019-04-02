71818f7b28e137e2bb6d1649adcf3cd81a9270d4
Emile Fokkema <emilefokkema@gmail.com>
2019-04-02 16:13:34 +0200

    fix the fb and twitter links

--- js/newtab.js
+++ js/newtab.js
@@ -4,13 +4,45 @@ var f = function(require){
 	var configProvider = require("./config-provider");
 	var Vue = require("vue");
 
-	var draw = function(width, height, canvasElement){
-		var canvas = new CanvasWithSize(canvasElement, width, height);
-		var instruction = Instruction.createForCanvas(canvas, configProvider.getConfig());
-		console.log("https://emilefokkema.github.io/mondirandom/?i="+instruction.toString());
-	};
-
-	draw(window.innerWidth, window.innerHeight, document.getElementById("main_canvas"));
+	new Vue({
+		el: "#main",
+		data:function(){
+			return {shareLink:""}
+		},
+		mounted:function(){
+			var width = window.innerWidth,
+				height = window.innerHeight,
+				canvasElement = document.getElementById("main_canvas"),
+				canvas = new CanvasWithSize(canvasElement, width, height),
+				instruction = Instruction.createForCanvas(canvas, configProvider.getConfig());
+			this.shareLink = "https://emilefokkema.github.io/mondirandom/?i="+instruction.toString();
+		},
+		components:{
+			'share':{
+				props:{
+					sharelink:String
+				},
+				methods:{
+					shareUrl:function(url){
+						window.open(
+						  url,
+						  'Share',
+						  'menubar=no,toolbar=no,resizable=yes,scrollbars=yes,height=600,width=600'
+						);
+					},
+					shareFacebook:function(){
+						var facebookLink = "https://www.facebook.com/sharer/sharer.php?u="+this.sharelink;
+						this.shareUrl(facebookLink);
+					},
+					shareTwitter:function(){
+						var twitterLink = "https://twitter.com/intent/tweet?text=Enjoy%20this%20Mondirandom%20Painting%3A%20&amp;url="+this.sharelink;
+						this.shareUrl(twitterLink);
+					}
+				},
+				template:document.getElementById("share-template").innerHTML
+			}
+		}
+	});
 }
 
 if(typeof module !== "undefined" && typeof require == "function"){
