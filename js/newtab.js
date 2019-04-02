d1880427efa80a3bc789596888245c56d62e3a06
Emile Fokkema <emilefokkema@gmail.com>
2019-04-02 15:27:20 +0200

    add vue

--- js/newtab.js
+++ js/newtab.js
@@ -2,6 +2,7 @@ var f = function(require){
 	var Instruction = require("./instruction");
 	var CanvasWithSize = require("./canvas-with-size");
 	var configProvider = require("./config-provider");
+	var Vue = require("vue");
 
 	var draw = function(width, height, canvasElement){
 		var canvas = new CanvasWithSize(canvasElement, width, height);
@@ -16,5 +17,10 @@ if(typeof module !== "undefined" && typeof require == "function"){
 	module.exports = f(require);
 }
 else if(typeof define == "function" && typeof requirejs !== "undefined"){
+	requirejs.config({
+		paths:{
+			vue:["../node_modules/vue/dist/vue"]
+		}
+	});
 	define(f);
 }
