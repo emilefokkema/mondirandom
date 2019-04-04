783b798219084fbb963e1ccfb50f73e146b2e542
Emile Fokkema <emilefokkema@gmail.com>
2019-04-04 14:17:17 +0200

    create the RandomValueProvider at a higher level

--- js/newtab.js
+++ js/newtab.js
@@ -2,10 +2,12 @@ var f = function(require){
 	var Instruction = require("./instruction");
 	var CanvasWithSize = require("./canvas-with-size");
 	var configProvider = require("./config-provider");
+	var RandomValueProvider = require("./random-value-provider");
 
 	var draw = function(width, height, canvasElement){
 		var canvas = new CanvasWithSize(canvasElement, width, height);
-		var instruction = Instruction.createForCanvas(canvas, configProvider.getConfig());
+		var config = configProvider.getConfig();
+		var instruction = Instruction.createForCanvas(canvas, config, new RandomValueProvider(config.random));
 		console.log("https://emilefokkema.github.io/mondirandom/?i="+instruction.toString());
 	};
 
